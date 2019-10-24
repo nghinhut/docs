08ac7d1d62a589afbe9f0f8049b89a2c670d3df2
Shirou <nghinhut@gmail.com>
2019-10-24 20:57:58 +0700

    Update CI

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,5 +1,11 @@
 shopt -s extglob
 
+./.scripts/prebuild.sh
+
 rm -rf dist/
 mkdir -p dist/
-cp -r !(dist) dist
+cp -r !(dist|*.zip) dist
+./.scripts/plantuml-build.sh
+
+rm artifacts.zip
+cd dist/ && zip -r ../artifacts.zip ./*
