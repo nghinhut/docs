333b5376e7dfa7272e468906766a4724ccf2af78
Shirou <nghinhut@gmail.com>
2019-11-02 00:26:09 +0700

    fix(no release): ???

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,10 +2,12 @@ shopt -s extglob
 
 ./.scripts/prebuild.sh
 
+
 rm -rf dist/
 mkdir -p dist/
 cp -r !(dist|*.zip) dist
 ./.scripts/plantuml-build.sh
+
 
 rm -f artifacts.zip
 cd dist/ && zip -r ../artifacts.zip ./*
