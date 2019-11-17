8fc9d0d1f410045688894a1e5b4cc467826828eb
Shirou <nghinhut@gmail.com>
2019-11-17 17:42:06 +0700

    ci(pages): update build script

--- scripts/hugo-build.sh
+++ scripts/hugo-build.sh
@@ -1,4 +1,10 @@
-HUGO=$PWD/scripts/hugo_0.59.0
+## Cleanup all files in public/ but keep some.
+shopt -s extglob
+mkdir -p public
+cd public && rm -vr !(plantuml|LICENSE|README.md) && cd ..
 
+
+## Hugo build
+HUGO=$PWD/scripts/hugo_0.59.0
 $HUGO
 cp -R assets/ ./public/
--- scripts/plantuml-build.sh
+++ scripts/plantuml-build.sh
@@ -2,11 +2,7 @@
 
 shopt -s extglob
 
-## Configuration
 OUTPUT_DIR=${1:-public}
-PLANTUML=scripts/plantuml.1.2019.11.jar
-SOURCES_FILES=$(find "$OUTPUT_DIR" -iname '*.puml' | grep -v static)
-
 
 ## Pre-build
 ./scripts/plantuml-header-patcher.sh
@@ -14,9 +10,11 @@ SOURCES_FILES=$(find "$OUTPUT_DIR" -iname '*.puml' | grep -v static)
 ### Prepare public/
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
-cp -r !(public|*.zip|posts|assets|scripts|web|protos) "$OUTPUT_DIR"
+cp -r !(public|*.zip|posts|assets|scripts|web|protos|config.yaml) "$OUTPUT_DIR"
 
 ## Build
+PLANTUML=scripts/plantuml.1.2019.11.jar
+SOURCES_FILES=$(find "$OUTPUT_DIR" -iname '*.puml' | grep -v static)
 for file in ${SOURCES_FILES}; do
     printf "Build %s... " "$file"
     java -DPLANTUML_LIMIT_SIZE=8192 -jar $PLANTUML -charset UTF-8 -progress "$file" -tsvg
